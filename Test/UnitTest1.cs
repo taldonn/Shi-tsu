0bbe87cbfdbd803ec5e579f1a074ae183426c0a4
taldonn <sean.whetten@gmail.com>
2011-02-25 18:02:36 -0700

    Do you know what i know

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Shi_tsu;
+using NUnit.Framework;
+using System.Diagnostics;
+using System.IO;
+
+namespace Test
+{
+    [TestFixture]
+    public class UnitTest : FolderWatch 
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Stream s = Console.OpenStandardOutput();
+
+        }
+    }
+}
